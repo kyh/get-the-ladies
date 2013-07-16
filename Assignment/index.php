d2bdeafcf68fef8c45ba28a6d7c0b2a50fbcd74c
raiedsiddiqui <raiedthegreat1@gmail.com>
2013-07-15 21:51:58 -0400

    Commented Code, Added Categories, Updated XML syntax

--- Assignment/index.php
+++ Assignment/index.php
@@ -1,6 +1,6 @@
 <?php
 	$pickUp = simplexml_load_file('pickUpLines.xml');
-	$rand = rand(0, 5); //random number from 0-5
+	$rand = rand(0, 59); //random number from 0-60
 
 	if ( $_GET['category'] && $_GET['category'] != '' ) {
 		$category = $_GET['category'];
@@ -15,18 +15,18 @@
 		}
 	}
 
+	// Code Below checks if the user has checked the "like" check box. 
 	if ($_GET['likebox'] && $_GET['likebox'] == 'ilike') {
 		$likedline = $_GET['pickupline'];
 		savetofile($likedline);
-
 		echo("SAVED");
 
 	}
 
+	//Function takes the pickup up and saves it to a file. 
 	function savetofile ($pline) {
 		$file="saved.txt";
-		file_put_contents($file, $pline, FILE_APPEND | LOCL_EX);
-		
+		file_put_contents($file, $pline, FILE_APPEND | LOCL_EX);	
 	}
 
 ?>
@@ -44,6 +44,9 @@
 		<?php
 			if($flag) {
 				// echo ('A Radio Button is clicked!');
+				
+				// Code below is triggered if a category is chosen. It then goes through the xml file and saves all jokes 
+				// that fit in that category. It then spits out a random joke form that category. 
 				$categorized = array();
 
 				for ($x=0; $x<=count($pickUp); $x++)
@@ -61,18 +64,21 @@
 				echo ('<input type="text" name="pickupline" size="150" value="' . $categorized[$rand1] . '">');
 				
 			}else {	
-
-
+				//Displays ALL pickup lines. 
 				echo('<input type="text" name="pickupline" size="150" value="' . $pickUp->joke[$rand]->title . '">');
 
 			}
 		?>
 
-	
+		<br>
 		<input type="radio" name="category" value="all" <?php if ($category=="all") echo 'checked="checked"'?>  /> All<br>
 		<input type="radio" name="category" value="nerdy" <?php if ($category=="nerdy") echo 'checked="checked"'?> /> Nerdy Pickup Lines<br>
 		<input type="radio" name="category" value="cheesy" <?php if ($category=="cheesy") echo 'checked="checked"'?> />Cheesy Pickup Lines <br>
-		<input type="radio" name="category" value="Ridiculous" <?php if ($category=="Ridiculous") echo 'checked="checked"'?> />Ridiculous Pickup Lines <br>
+		<input type="radio" name="category" value="Gross" <?php if ($category=="Gross") echo 'checked="checked"'?> />Gross Pickup Lines <br>
+		<input type="radio" name="category" value="Rude" <?php if ($category=="Rude") echo 'checked="checked"'?> />Rude Pickup Lines <br>
+		<input type="radio" name="category" value="Valentines" <?php if ($category=="Valentines") echo 'checked="checked"'?> />Valentines Pickup Lines <br>
+		<input type="radio" name="category" value="Disney" <?php if ($category=="Disney") echo 'checked="checked"'?> />Disney Pickup Lines <br>
+
 		<input type="submit" value="New Line">
 		<input type="checkbox" name="likebox" value="ilike"> I like this! <br>
 
