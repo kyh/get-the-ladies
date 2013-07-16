e2214dab8bae020f9e9865e598cf7fd51b4c3c78
Kai Yu Hsu <im.kaiyu@gmail.com>
2013-07-16 18:52:49 -0400

    Combined front and backend

--- Assignment/index.php
+++ Assignment/index.php
@@ -1,12 +1,8 @@
 <?php
 	$pickUp = simplexml_load_file('pickUpLines.xml');
 	$rand = rand(0, 59); //random number from 0-60
-
 	if ( $_GET['category'] && $_GET['category'] != '' ) {
 		$category = $_GET['category'];
-	
-		echo("$category");
-
 		if ($category=="all") {
 			$flag=false;
 		}
@@ -14,15 +10,12 @@
 			$flag = true;
 		}
 	}
-
 	// Code Below checks if the user has checked the "like" check box. 
 	if ($_GET['likebox'] && $_GET['likebox'] == 'ilike') {
 		$likedline = $_GET['pickupline'];
 		savetofile($likedline);
 		echo("SAVED");
-
 	}
-
 	//Function takes the pickup up and saves it to a file. 
 	function savetofile ($pline) {
 		$file="saved.txt";
@@ -31,67 +24,83 @@
 
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>How to get the ladies.</title>
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<form>
-	<section id="main">
-		<?php
-			if($flag) {
-				// echo ('A Radio Button is clicked!');
-				
-				// Code below is triggered if a category is chosen. It then goes through the xml file and saves all jokes 
-				// that fit in that category. It then spits out a random joke form that category. 
-				$categorized = array();
-
-				for ($x=0; $x<=count($pickUp); $x++)
-				{
-					if ($pickUp->joke[$x]->category == "$category")
-					{
-						$line = $pickUp->joke[$x]->title;
-						array_push($categorized, $line);
-
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<title>How to get the Ladies</title>
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	<body>
+		<div class="buttonContainer">
+			<!-- Class "menu-open" gets applied to menu -->
+			<button id="showMenu"><div class="arrow" aria-hidden="true" data-icon="&#xe104;"></div></button>
+		</div>
+		<nav class="saveMenu" id="saveMenu">
+			<ul>
+				<li>Save</li>
+				<li>Show</li>
+			</ul>
+		</nav>
+		<div class="saveContainer">
+			<button id="showSaveMenu"><div class="save" aria-hidden="true" data-icon="&#xe06a;"></div></button>
+		</div>
+		<section class="main">
+			<form>
+				<nav class="menu" id="menu">
+				<h3>Categories</h3>
+					<input type="radio" name="category" value="all" id="allCat"><label for="allCat">All</label>
+					<input type="radio" name="category" value="cheesy" id="cheesyCat"><label for="cheesyCat">Cheesy</label>
+					<input type="radio" name="category" value="Disney" id="disCat"><label for="disCat">Disney</label>
+					<input type="radio" name="category" value="Gross" id="grossCat"><label for="grossCat">Gross</label>
+					<input type="radio" name="category" value="nerdy" id="nerdCat"><label for="nerdCat">Nerdy</label>
+					<input type="radio" name="category" value="Rude" id="rudeCat"><label for="rudeCat">Rude</label>
+					<input type="radio" name="category" value="Valentines" id="valCat"><label for="valCat">Valentines</label>
+				</nav>
+				<?php
+					if($flag) {
+						// echo ('A Radio Button is clicked!');
+						// Code below is triggered if a category is chosen. It then goes through the xml file and saves all jokes 
+						// that fit in that category. It then spits out a random joke form that category. 
+						$categorized = array();
+							for ($x=0; $x<=count($pickUp); $x++) {
+								if ($pickUp->joke[$x]->category == "$category") {
+									$line = $pickUp->joke[$x]->title;
+									array_push($categorized, $line);
+								}
+							}
+						$rand1 = rand(0, count($categorized)-1);
+						echo ('<input type="text" readonly="readonly" name="pickupline" size="150" class="pickUpLine" value="' . $categorized[$rand1] . '">');
+							}else {	
+						//Displays ALL pickup lines. 
+						echo('<input type="text" readonly="readonly" name="pickupline" size="150" class="pickUpLine" value="' . $pickUp->joke[$rand]->title . '">');
 					}
-				}
+				?>
+				<button id="heartContainer">
+					<div id="heart"></div>
+				</button>
+			</form>
+		</section>
+		<!-- Classie - class helper functions by @desandro https://github.com/desandro/classie -->
+		<script src="js/classie.js"></script>
+		<script>
+			var menu = document.getElementById( 'menu' ),
+				savedMenu = document.getElementById( 'saveMenu' ),
+				showMenu = document.getElementById( 'showMenu' ),
+				showSaved = document.getElementById( 'showSaveMenu' ),
+				body = document.body;
 
-				$rand1 = rand(0, count($categorized)-1);
-				
-				echo ('<input type="text" name="pickupline" size="150" value="' . $categorized[$rand1] . '">');
-				
-			}else {	
-				//Displays ALL pickup lines. 
-				echo('<input type="text" name="pickupline" size="150" value="' . $pickUp->joke[$rand]->title . '">');
+			showMenu.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( menu, 'menu-open' );
+			};
 
-			}
-		?>
-
-		<br>
-		<input type="radio" name="category" value="all" <?php if ($category=="all") echo 'checked="checked"'?>  /> All<br>
-		<input type="radio" name="category" value="nerdy" <?php if ($category=="nerdy") echo 'checked="checked"'?> /> Nerdy Pickup Lines<br>
-		<input type="radio" name="category" value="cheesy" <?php if ($category=="cheesy") echo 'checked="checked"'?> />Cheesy Pickup Lines <br>
-		<input type="radio" name="category" value="Gross" <?php if ($category=="Gross") echo 'checked="checked"'?> />Gross Pickup Lines <br>
-		<input type="radio" name="category" value="Rude" <?php if ($category=="Rude") echo 'checked="checked"'?> />Rude Pickup Lines <br>
-		<input type="radio" name="category" value="Valentines" <?php if ($category=="Valentines") echo 'checked="checked"'?> />Valentines Pickup Lines <br>
-		<input type="radio" name="category" value="Disney" <?php if ($category=="Disney") echo 'checked="checked"'?> />Disney Pickup Lines <br>
-
-		<input type="submit" value="New Line">
-		<input type="checkbox" name="likebox" value="ilike"> I like this! <br>
-
-	</form>
-
-	<form action="saved.php"> 
-		<input type="submit" value="View Saved Lines">
-	</form>
-
-
-	
-
-
-	</section>
-</body>
+			showSaved.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( savedMenu, 'saveMenu-open' );
+			};
+		</script>
+	</body>
 </html>
